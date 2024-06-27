44a82c17db64cfe84150d0d6c25a9a841a5348eb
Aaron Chan <chan.aaron73@gmail.com>
2024-06-27 16:20:01 -0700

    Implemented AddStudent API

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 // Initialize express app
 const app = express();
+
+// Middleware
+app.use(bodyParser.json());
 
 // Routes
 app.use('/api', routes);
