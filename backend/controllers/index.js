7e7c61fbd22a3bc41ac1bd5231730576b96efcc5
Aaron Chan <chan.aaron73@gmail.com>
2024-07-03 15:12:43 -0700

    added endpoints for attempts

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,7 +1,9 @@
 const userController = require('./userController');
 const questionController = require('./questionController');
+const attemptController = require('./attemptController');
 
 module.exports = {
   userController,
   questionController,
+  attemptController,
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { userController, questionController } = require('../controllers')
+const { userController, questionController, attemptController } = require('../controllers')
 
 // Users
 router.post('/users/register', userController.AddStudent);
@@ -9,5 +9,9 @@ router.get('/users', userController.GetStudents);
 // Questions
 router.get('/questions', questionController.GetQuestions);
 router.get('/questions/:id', questionController.GetQuestionsById);
+
+// Attempts
+router.get('/attempts/:username', attemptController.GetAttemptsByUsername);
+router.post('/attempts/:attemptId', attemptController.AddAttempt);
 
 module.exports = router;
