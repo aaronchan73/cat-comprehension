1b7b0d45956a708e4a512df34479c1bda8eb8abd
Aaron Chan <chan.aaron73@gmail.com>
2024-07-03 15:11:40 -0700

    add attempts logic (TODO)

---
+++ backend/controllers/attemptController.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read Attempts.json and parse contents
+const readAttemptsJSON = () => {
+  const filePath = path.join(__dirname, '../Attempts.json');
+  const data = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(data);
+};
+
+// Write new data into Attempts.json
+const updateAttemptsJSON = (data) => {
+    const filePath = path.join(__dirname, '../Attempts.json');
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+};
+
+// Adds a user’s answer and performance to the corresponding code sample
+exports.AddAttempt = (req, res) => {
+    // TODO
+};
+
+// Gets a list of all the attempts in in the application for the username provided
+exports.GetAttemptsByUsername = (req, res) => {
+    const { username } = req.params.username;
+
+    // Get attempts and find the specific attempt matching the given username
+    const attempts = readAttemptsJSON();
+    const attempt = attempts.find(a => a.user === username);
+
+    if (attempt) {
+        res.status(200).json({ message: 'Attempt retrieved successfully', attempt });
+    } else {
+        res.status(400).json({ message: 'Attempt not found' });
+    }
+}
