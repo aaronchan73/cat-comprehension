4ddcb6d780a4289db860089d1af44ab185344e9b
samzhao273 <samzhao273@gmail.com>
2024-07-06 11:39:58 -0700

    added summary box + onchange for changing question

--- frontend/src/pages/student/exercisePage.tsx
+++ frontend/src/pages/student/exercisePage.tsx
@@ -1,45 +1,66 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CodeBox from '../../components/codeBox'
+import '../../styles/exercisePage.css'
 import { getExercises, getExerciseById } from '../../services/exercises';
+import SummaryBox from '../../components/summaryBox';
+import { Button, Tooltip } from '@mui/material';
 
 export default function ExercisePage() {
     const [code, setCode] = useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [questionId, setQuestionId] = useState<string>('1')
+
     const navigate = useNavigate()
 
     const getExercise = async () => {
-        try{
+        try {
             const data = await getExercises()
             console.log(data)
+            // case where we want to randomize the exercise
+            const randomNumber = Math.random()
             setCode(data.questions[0].code)
+            setName(data.questions[0].name)
         } catch (e) {
             console.log(e)
         }
     }
 
     const setExerciseById = async (id: string) => {
-        try{
+        try {
             const data = await getExerciseById(id)
             console.log(data)
             setCode(data.question.code)
+            setName(data.question.name)
         } catch (e) {
             console.log(e)
         }
-    
+
+    }
+
+    const handleChangeQuestion = () => {
+        const newQuestion = String(Number(questionId) + 1)
+        console.log(newQuestion)
+        setExerciseById(newQuestion)
     }
 
     useEffect(() => {
-        setExerciseById('2')
-    }, [code])
+        setExerciseById(questionId)
+    }, [code, questionId])
 
     return (
-        <div>
-            <h1>
-                Exercise Page
-            </h1>
-            <CodeBox language="python" code={code} />
+        <div className='exercisePage'>
+            <div className='exerciseContainer'>
+                <div>
+                    <Tooltip title="Click title to change question" arrow>
+                        <Button style={{ color: 'black' }} onChange={() => handleChangeQuestion()}>{name}</Button>
+                    </Tooltip>                    
+                    <CodeBox language="python" code={code} name={name} />
+                </div>
+                <SummaryBox />
+            </div>
         </div>
     )
-    
+
 
 }
