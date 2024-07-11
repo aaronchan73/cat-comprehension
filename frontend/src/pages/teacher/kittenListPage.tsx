0e12ce8258629eda3dcb0cafa12b3ccfea7a6deb
UBC Student <lzhang74@students.cs.ubc.ca>
2024-07-10 20:54:32 -0700

    fixed nits

--- frontend/src/pages/teacher/kittenListPage.tsx
+++ frontend/src/pages/teacher/kittenListPage.tsx
@@ -55,7 +55,7 @@ export default function KittenListPage() {
 
   /**
    * @description - handle button click to fetch attempts for a selected user
-   *  @param username - username of selected user
+   * @param username - username of selected user
    */
   const handleButtonClick = async (username: string) => {
     setSelectedUsername(username);
@@ -79,7 +79,7 @@ export default function KittenListPage() {
 
   /**
    * @description - get the current attempt for the selected question
-   *  @param questionId - id of the exercise
+   * @param questionId - id of the exercise
    */
   const getCurrentAttempt = (questionId: number) => {
     if (!attempts.userAttempts || attempts.userAttempts.length === 0) {
@@ -107,10 +107,7 @@ export default function KittenListPage() {
       }
     );
   };
-
-  /**
-   * @description - get the current question and attempt
-   */
+  
   const currentQuestion = questions[questionIndex];
   const currentAttempt = currentQuestion
     ? getCurrentAttempt(currentQuestion.id)
