2a67808e2fe3b77f7b7dd3d49bba8ab882534569
Timothy Fang <timothyfang@Timothys-MacBook-Pro.local>
2024-07-30 20:42:31 -0700

    added testing

--- backend/test/attemptController.test.mjs
+++ backend/test/attemptController.test.mjs
@@ -1,5 +1,5 @@
-import { expect } from "chai";
-import sinon from "sinon";
+// import { expect } from "chai";
+// import sinon from "sinon";
 import fs from "fs";
 const attemptController = await import("../controllers/attemptController.js");
 
